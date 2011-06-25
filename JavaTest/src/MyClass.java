a726f4b653f1fbeafe8a4f0fdd268060c16b6195
Lior Kiperman <HealingAura@gmail.com>
2011-06-25 12:25:45 +0300

    Test

---
+++ JavaTest/src/MyClass.java
@@ -0,0 +1,10 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: HealingAura
+ * Date: 6/25/11
+ * Time: 12:25 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MyClass
+{
+}
