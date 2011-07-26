8afa57f66d96dd2c280c126864f687e682e041b4
Yoac Shavit <shavity3@yahoo.com>
2011-07-26 05:40:10 -0400

    Testing if the GUI works

--- WindowGUI/src/WindowGUI.java
+++ WindowGUI/src/WindowGUI.java
@@ -1,27 +1,66 @@
+import javax.swing.GroupLayout;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.LayoutStyle;
+
+import javax.swing.WindowConstants;
+import javax.swing.SwingUtilities;
 
 
 /**
- * @author HealingAura
- * @version 1.0
- * @created 30-Apr-2011 1:18:16 PM
- */
-public abstract class WindowGUI {
-
-	public ShowFiles m_ShowFiles;
-	public UserAuthentication m_UserAuthentication;
-
-	public WindowGUI(){
-
-	}
-
-	public void finalize() throws Throwable {
-
-	}
+* This code was edited or generated using CloudGarden's Jigloo
+* SWT/Swing GUI Builder, which is free for non-commercial
+* use. If Jigloo is being used commercially (ie, by a corporation,
+* company or business for any purpose whatever) then you
+* should purchase a license for each developer using Jigloo.
+* Please visit www.cloudgarden.com for details.
+* Use of Jigloo implies acceptance of these licensing terms.
+* A COMMERCIAL LICENSE HAS NOT BEEN PURCHASED FOR
+* THIS MACHINE, SO JIGLOO OR THIS CODE CANNOT BE USED
+* LEGALLY FOR ANY CORPORATE OR COMMERCIAL PURPOSE.
+*/
+public abstract class WindowGUI extends javax.swing.JFrame {
+	private JButton TestButton;
 
 	/**
-	 * 
-	 * @param files
+	 * @author HealingAura
+	 * @version 1.0
+	 * @created 30-Apr-2011 1:18:16 PM
 	 */
+	
+	//public ShowFiles m_ShowFiles;
+	//public UserAuthentication m_UserAuthentication;
+		
+	public WindowGUI() {
+		super();
+		initGUI();
+	}
+	
+	private void initGUI() {
+		try {
+			GroupLayout thisLayout = new GroupLayout((JComponent)getContentPane());
+			getContentPane().setLayout(thisLayout);
+			setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+			{
+				TestButton = new JButton();
+				TestButton.setText("Test Button");
+			}
+			thisLayout.setVerticalGroup(thisLayout.createSequentialGroup()
+				.addContainerGap(126, 126)
+				.addComponent(TestButton, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE)
+				.addContainerGap(122, Short.MAX_VALUE));
+			thisLayout.setHorizontalGroup(thisLayout.createSequentialGroup()
+				.addContainerGap(144, 144)
+				.addComponent(TestButton, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE)
+				.addContainerGap(179, Short.MAX_VALUE));
+			pack();
+			setSize(400, 300);
+		} catch (Exception e) {
+		    //add your error handling code here
+			e.printStackTrace();
+		}
+	}
+	
 	public abstract void UpdateWindow(String[] files);
 
 	public abstract void UpdateWindow();
